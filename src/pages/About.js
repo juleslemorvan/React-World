00aebe57a6f588da0b7527c3505600388ba2161b
Le Morvan <jules.lemorvan01@gmail.com>
2022-01-12 22:09:40 +0100

    navigation OK

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>1er projet en React</p>
+      <br />
+    </div>
+  );
+};
+
+export default About;
